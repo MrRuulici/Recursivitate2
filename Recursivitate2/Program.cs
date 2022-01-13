23ad0b5acffe503f2538f1301979785515ea4aeb
Raul Ulici <raululici@yahoo.com>
2022-01-13 16:25:50 +0200

    final

---
+++ Recursivitate2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Recursivitate2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[] { 5, 6, 7, 8, 3, 2, 10, 2 };
+            Console.WriteLine(Recursiv.Maxim(v, v.Length-1, int.MinValue)) ;
+        }
+    }
+}
---
+++ Recursivitate2/Recursiv.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivitate2
+{
+    class Recursiv
+    {
+        public static int Maxim(int[] v, int n, int mx)
+        {
+            if (n == 0)
+            {
+                if (mx >= v[n])
+                    return mx;
+                else return v[n];
+            }
+            else
+            {
+                if (mx >= v[n])
+                    return Maxim(v, n - 1, mx);
+                else
+                    return Maxim(v, n - 1, v[n]);
+            }
+        }
+    }
+}
